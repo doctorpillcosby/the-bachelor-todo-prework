d8a4ee6fbc6b8c00bd2ed31f8f1da5aacfb8c92c
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-11 21:52:18 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,4 +45,16 @@
  end	 
 
 
-
+def get_occupation(data, hometown)
+   # code here	   # code here
+    data.each do |season, array|
+      array.each do |hash|
+        hash.each do |k,v|
+          if v == hometown
+            return hash["occupation"]
+          end
+        end
+      end
+    end
+  
+ end	
